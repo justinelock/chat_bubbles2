3121fd803371ebc020c45c8e68433796134e628f
tre <tre>
2023-02-26 16:12:11 +0800

    add 3 components

--- lib/chat_bubbles.dart
+++ lib/chat_bubbles.dart
@@ -3,6 +3,9 @@ library chat_bubbles;
 ///
 /// all the widgets of the package export here
 /// CHAT BUBBLES
+/// [BubbleNormalText]
+/// [BubbleNormalTextFile]
+/// [BubbleNormalTextImage]
 /// [BubbleNormal]
 /// [BubbleSpecialOne]
 /// [BubbleSpecialTwo]
@@ -27,3 +30,8 @@ export 'bubbles/bubble_special_three.dart';
 export 'date_chips/date_chip.dart';
 export 'message_bars/message_bar.dart';
 export 'bubbles/bubble_normal_image.dart';
+export 'bubbles/bubble_normal_text.dart';
+export 'bubbles/bubble_normal_text_image.dart';
+export 'bubbles/bubble_normal_text_file.dart';
+export 'models/meta_json.dart';
+export 'models/conversation_item.dart';
